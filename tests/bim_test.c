d3cdf81b82f831796a051d92a6d657a141fb1c17
Mattias Franberg <mattias.franberg@gmail.com>
2011-12-19 13:42:53 +0100

    Added unit tests for bim and fam parsers.

---
+++ tests/bim_test.c
@@ -0,0 +1,123 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#include <cmockery.h>
+
+#include <bim.h>
+#include <bim.c>
+
+/**
+ * Number of loci to parse.
+ */
+#define NUM_LOCI 2
+
+/**
+ * Index of locus to be returned by mock_fgets.
+ */
+int g_locus_index = 0;
+
+/**
+ * Mocks feof, returns 1 when g_locus_index is >= NUM_LOCI. 
+ */
+int
+mock_feof(FILE *stream)
+{
+    return g_locus_index <= NUM_LOCI;
+}
+
+/**
+ * Mock fgets, returns a locus as long as it has been called less than
+ * NUM_LOCI times.
+ *
+ * Note: You have to reset g_locus_index if you want to use this in
+ *       multiple tests.
+ */
+char *
+mock_fgets(char *s, int n, FILE *stream)
+{
+    const char *locus_list[NUM_LOCI] = { "1 rs1 0 1234567 A C", "1 rs2 0 7654321 G T" };
+
+    if( g_locus_index < NUM_LOCI )
+    {
+        strncpy( s, locus_list[ g_locus_index ], n );
+        return (char *) locus_list[ g_locus_index++ ];
+    }
+    else
+    {
+        return NULL;
+    }
+
+}
+
+/**
+ * Tests the parsing of a correct locus.
+ */
+void
+test_parse_locus(void **state)
+{
+    const char *TEST_STRING = "1 rs1 0 1234567 A C";
+    
+    struct pio_locus_t locus;
+    assert_int_equal( parse_locus( TEST_STRING, &locus ), PIO_OK );
+    assert_int_equal( locus.chromosome, 1 ); 
+    assert_string_equal( locus.name, "rs1" );
+    assert_int_equal( locus.position, 0 );
+    assert_int_equal( locus.bp_position, 1234567 );
+    assert_int_equal( locus.major, 'A' );
+    assert_int_equal( locus.minor, 'C' );
+}
+
+/**
+ * Tests that parsing fails when a field is missing.
+ */
+void test_parse_locus_fail(void **state)
+{
+    const char *TEST_STRING = "1 rs1 0 0 A";
+    
+    struct pio_locus_t locus;
+    assert_int_equal( parse_locus( TEST_STRING, &locus ), PIO_ERROR );
+}
+
+/**
+ * Tests the parsing of multiple loci. Since parse_loci uses
+ * IO functions, we need to have mocked versions for these.
+ */
+void
+test_parse_multiple_loci(void **state)
+{
+    struct pio_bim_file_t bim_file;
+    assert_int_equal( parse_loci( &bim_file ), PIO_OK );
+    assert_int_equal( bim_file.num_locus, NUM_LOCI );
+
+    struct pio_locus_t locus = bim_file.locus[0];
+    assert_int_equal( locus.chromosome, 1 ); 
+    assert_string_equal( locus.name, "rs1" );
+    assert_int_equal( locus.position, 0 );
+    assert_int_equal( locus.bp_position, 1234567 );
+    assert_int_equal( locus.major, 'A' );
+    assert_int_equal( locus.minor, 'C' );
+
+    locus = bim_file.locus[1];
+    assert_int_equal( locus.chromosome, 1 ); 
+    assert_string_equal( locus.name, "rs2" );
+    assert_int_equal( locus.position, 0 );
+    assert_int_equal( locus.bp_position, 7654321 );
+    assert_int_equal( locus.major, 'G' );
+    assert_int_equal( locus.minor, 'T' );
+}
+
+int main(int argc, char* argv[])
+{
+    const UnitTest tests[] = {
+        unit_test( test_parse_locus ),
+        unit_test( test_parse_locus_fail ),
+        unit_test( test_parse_multiple_loci ),
+    };
+
+    return run_tests( tests );
+}
---
+++ tests/fam_test.c
@@ -0,0 +1,142 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#include <cmockery.h>
+
+#include <fam.h>
+#include <fam.c>
+
+/**
+ * Number of samples to parse.
+ */
+#define NUM_SAMPLES 2
+
+/**
+ * Index of sample to be returned by mock_fgets.
+ */
+int g_sample_index = 0;
+
+/**
+ * Mocks feof, returns 1 when g_sample_index is >= NUM_SAMPLES. 
+ */
+int
+mock_feof(FILE *stream)
+{
+    return g_sample_index <= NUM_SAMPLES;
+}
+
+/**
+ * Mock fgets, returns a sample as long as it has been called less than
+ * NUM_SAMPLES times.
+ *
+ * Note: You have to reset g_sample_index if you want to use this in
+ *       multiple tests.
+ */
+char *
+mock_fgets(char *s, int n, FILE *stream)
+{
+    const char *sample_list[NUM_SAMPLES] = { "0 P1 0 0 0 0", "0 P2 0 0 0 0" };
+
+    if( g_sample_index < NUM_SAMPLES )
+    {
+        strncpy( s, sample_list[ g_sample_index ], n );
+        return (char *) sample_list[ g_sample_index++ ];
+    }
+    else
+    {
+        return NULL;
+    }
+
+}
+
+/**
+ * Tests the parsing of a correct sample.
+ */
+void
+test_parse_sample(void **state)
+{
+    const char *TEST_STRING = "1 P1 0 0 1 0";
+    
+    struct pio_sample_t person;
+    assert_int_equal( parse_sample( TEST_STRING, &person ), PIO_OK );
+    assert_int_equal( person.fid, 1 ); 
+    assert_string_equal( person.iid, "P1" );
+    assert_int_equal( person.father_iid, 0 );
+    assert_int_equal( person.mother_iid, 0 );
+    assert_int_equal( person.sex, MALE );
+    assert_int_equal( person.phenotype.as_int, 0 );
+}
+
+/**
+ * Tests the parsing of a correct sample with a
+ * real phenotype.
+ */
+void test_parse_sample_double(void **state)
+{
+    const char *TEST_STRING = "1 P1 0 0 1 4.5";
+    
+    struct pio_sample_t person;
+    assert_int_equal( parse_sample( TEST_STRING, &person ), PIO_OK );
+    assert_int_equal( person.fid, 1 ); 
+    assert_string_equal( person.iid, "P1" );
+    assert_int_equal( person.father_iid, 0 );
+    assert_int_equal( person.mother_iid, 0 );
+    assert_int_equal( person.sex, MALE );
+    assert_true( fabs( person.phenotype.as_float - 4.5 ) <= 1e-6 );
+}
+
+/**
+ * Tests that parsing fails when a field is missing.
+ */
+void test_parse_sample_fail(void **state)
+{
+    const char *TEST_STRING = "1 P1 0 0 1";
+    
+    struct pio_sample_t person;
+    assert_int_equal( parse_sample( TEST_STRING, &person ), PIO_ERROR );
+}
+
+/**
+ * Tests the parsing of multiple samples. Since parse_samples uses
+ * IO functions, we need to have mocked versions for these.
+ */
+void
+test_parse_multiple_samples(void **state)
+{
+    struct pio_fam_file_t fam_file;
+    assert_int_equal( parse_samples( &fam_file ), PIO_OK );
+    assert_int_equal( fam_file.num_samples, NUM_SAMPLES );
+
+    struct pio_sample_t person = fam_file.sample[0];
+    assert_int_equal( person.fid, 0 ); 
+    assert_string_equal( person.iid, "P1" );
+    assert_int_equal( person.father_iid, 0 );
+    assert_int_equal( person.mother_iid, 0 );
+    assert_int_equal( person.sex, FEMALE );
+    assert_int_equal( person.phenotype.as_int, 0 );
+
+    person = fam_file.sample[1];
+    assert_int_equal( person.fid, 0 ); 
+    assert_string_equal( person.iid, "P2" );
+    assert_int_equal( person.father_iid, 0 );
+    assert_int_equal( person.mother_iid, 0 );
+    assert_int_equal( person.sex, FEMALE );
+    assert_int_equal( person.phenotype.as_int, 0 );
+}
+
+int main(int argc, char* argv[])
+{
+    const UnitTest tests[] = {
+        unit_test( test_parse_sample ),
+        unit_test( test_parse_sample_double ),
+        unit_test( test_parse_sample_fail ),
+        unit_test( test_parse_multiple_samples ),
+    };
+
+    return run_tests( tests );
+}
